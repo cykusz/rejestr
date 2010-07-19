e220d54eef0f9cf86d74bcc82468486e75007878
Rafal M <kruk87@gmail.com>
2010-07-19 22:06:53 +0200

    first commit

---
+++ MainWindow.cpp
@@ -0,0 +1,18 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent){
+    ui.setupUi(this);
+}
+
+void MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui.retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ rejestr.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-07-19T22:06:15
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = rejestr
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp
+
+HEADERS  += MainWindow.h
+
+FORMS    += MainWindow.ui
