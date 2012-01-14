607f8b149d7e646bf66401b42817fbbc4556e8af
Rafal Mielniczuk <rafalm3@gmail.com>
2012-01-14 22:48:40 +0100

    test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     //qDebug() << QStyleFactory::keys();
     //QApplication::setStyle("Plastique");
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w;//ss
     w.show();
     return a.exec();//test
 }
